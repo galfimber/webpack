3c08fb6e6a9c6fd9e2ef5fdd96b3cc200c34ffbe
Nikita Varankin <galfimber2014@yandex.ru>
2023-12-17 15:20:52 +0500

    Сборка webpack

---
+++ src/modules/one.js
@@ -0,0 +1,6 @@
+const one = () => {
+  const one = "one.js";
+  console.log(one);
+};
+
+export default one;
---
+++ src/modules/two.js
@@ -0,0 +1,6 @@
+const two = () => {
+  const two = "two.js";
+  console.log(two);
+};
+
+export default two;
